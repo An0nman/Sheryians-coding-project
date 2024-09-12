6248bb1123fe73f012df963fb536705d35369f18
An0nman <03334121a@gmail.com>
2024-09-12 11:27:56 +0500

    DOM 2

---
+++ 15 project (DOM)/script.js
@@ -0,0 +1,9 @@
+var btn = document.querySelector("button");
+var fs = document.querySelector("#fs");
+var modal = document.querySelector("#modal");
+
+
+btn.addEventListener("click", function () {
+  fs.style.opacity="1";
+  modal.style.top="15%";
+});
