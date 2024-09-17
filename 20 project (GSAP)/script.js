1da6bd976ced53472807b7eec57eb6d2fbb23dc0
An0nman <03334121a@gmail.com>
2024-09-17 20:58:22 +0500

    gsap project

--- 20 project (GSAP)/script.js
+++ 20 project (GSAP)/script.js
@@ -1,3 +1,4 @@
+//menu
 var menu_btn = document.querySelector("#nav-part2>i");
 var full_scr_nav = document.querySelector("#full-scr-nav");
 var flag = 0;
@@ -17,3 +18,46 @@ menu_btn.addEventListener("click", function () {
     flag=0
   }
 });
+
+var tl=gsap.timeline();
+
+tl.from("#page1 h1",{
+  opacity:0,
+  y:60,
+  duration:0.6,
+})
+.from("#page1 h2",{
+  opacity:0,
+  y:50,
+  duration:0.5,
+  delay:-0.2,
+})
+.from("#page1 h3",{
+  opacity:0,
+  y:50,
+  duration:0.5,
+  delay:-0.2,
+})
+gsap.to("#page2 img",{
+  scale:.98,
+  scrollTrigger:{
+    trigger:"#page2 img",
+    scroll:"body",
+    // markers:true,
+    start:"top 80%",
+    end:"top 0%",
+    scrub:3
+  }
+})
+gsap.from("#page2 h1",{
+  rotateX:"-90deg",
+  opacity:0,
+  scrollTrigger:{
+    trigger:"#page2 h1",
+    scroller:"body",
+    markers:true,
+    start:"top 70%",
+    end:"top 60%",
+    scrub:1,
+  }
+})
